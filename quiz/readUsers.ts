570a0bd438d28bd2dc63418109556a13335d5e4b
varunsai <varunsai855@gmail.com>
2025-02-17 12:41:47 -0500

    Express

--- quiz/readUsers.ts
+++ quiz/readUsers.ts
@@ -1,2 +1,20 @@
-import express, { Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import {UserRequest} from "./types";
+
 const router = express.Router();
+
+router.get('/username/:name', (req: UserRequest, res: Response) => {
+    let name = req.params.name;
+    let user = req.users?.filter(us => us.username === name);
+    return res.send(user);
+});
+
+// a route that sends the usernames of the users to the client
+router.get('/usernames', (req: UserRequest, res: Response) => {
+    let usernames = req.users?.map((user) => {
+        return { id: user.id, username: user.username };
+    });
+    res.send(usernames);
+});
+
+export default router;
